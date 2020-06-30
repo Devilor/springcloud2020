8e3cb8e771e2a22fd929cf92a58b04e21a27b794
Ernesto <daike12123@outlook.com>
2020-06-30 17:09:17 +0800

    Eureka 服务发现

--- cloud-provider-payment8001/src/main/java/com/vat/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/vat/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import com.vat.springcloud.entities.Payment;
 import com.vat.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author 东街浊酒づ
@@ -19,6 +22,12 @@ public class PaymentController {
 
     @Resource
     private PaymentService paymentService;
+
+    /**
+     * 服务发现 Bean
+     */
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @Value("${server.port}")
     private String serverPort;
@@ -41,5 +50,23 @@ public class PaymentController {
             return new CommonResult(999, "查询失败！");
         }
         return new CommonResult(999, "查询成功！ServerPort:" + serverPort, payment);
+    }
+
+    @GetMapping(value = "/vat/discovery")
+    public Object discovery() {
+        //注册的服务-provider 获取注册中心的注册信息
+        List<String> services = this.discoveryClient.getServices();
+        for (int i = 0, size = services.size(); i < size; i++) {
+            log.info("Provider,Server Port:{},获取注册中心的服务信息：{}", this.serverPort, services.get(i));
+        }
+        //根据某个在 Eureka 中注册的服务，获取该服务的配置信息，这里以“CLOUD-PAYMENT-SERVICE”注册的服务为例
+        List<ServiceInstance> instances = this.discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (int i = 0, size = instances.size(); i < size; i++) {
+            ServiceInstance serviceInstance = instances.get(i);
+            log.info("CLOUD-PAYMENT-SERVICE 服务的信息有：{}",
+                serviceInstance.getServiceId() + "\t" + serviceInstance.getHost() + "\t" + serviceInstance.getPort()
+                    + "\t" + serviceInstance.getUri());
+        }
+        return this.discoveryClient;
     }
 }

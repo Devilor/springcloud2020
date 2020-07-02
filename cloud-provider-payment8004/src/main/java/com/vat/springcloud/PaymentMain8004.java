840d5f82f2001eefcdc02206d1a8f78bf99ab256
Ernesto <daike12123@outlook.com>
2020-07-02 15:28:56 +0800

    Zookeeper 替换 Eureka.

---
+++ cloud-provider-payment8004/src/main/java/com/vat/springcloud/PaymentMain8004.java
@@ -0,0 +1,17 @@
+package com.vat.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author 东街浊酒づ
+ * @date 2020/7/2
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+}
---
+++ cloud-provider-payment8004/src/main/java/com/vat/springcloud/controller/PaymentController.java
@@ -0,0 +1,25 @@
+package com.vat.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @author 东街浊酒づ
+ * @date 2020/7/2
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/zk")
+    public String paymentZk() {
+        return "SpringCloud With Zookeeper : " + serverPort + UUID.randomUUID().toString();
+    }
+}
